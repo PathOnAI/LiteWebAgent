437889342539b1cfaa4d73e10fb78230e6b18d84
Tata0703 <danqing.zhang.personal@gmail.com>
2024-07-21 16:47:03 -0700

    remove cache, update readme

--- auto-interact/cache/YpKdJX.py
+++
@@ -1,4 +0,0 @@
-async def search_test(page):
-    await page.wait_for_load_state('domcontentloaded')
-    await page.fill("#twotabsearchtextbox", "dining table")
-    await page.click("#nav-search-submit-button")
