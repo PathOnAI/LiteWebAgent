1c8b6e92a9fd99e64e3b13bedeaed0b8c1ab861c
thehunter911 <grand.hunter.dark.15@gmail.com>
2024-07-27 12:05:17 -0400

    f

--- personal-companion-upgraded/simulate.py
+++ personal-companion-upgraded/simulate.py
@@ -12,20 +12,36 @@ async def simulate():
     screen = ScreenAgent(page.current_page)
     execute = ExecuteAgent(page.current_page)
 
-    screen.set_url('https://www.discord.com/login')
+    screen.set_url('https://mail.google.com')
     await screen.read_page(navigate=True)
 
-    cache_dir = await screen.build_action('enter email as izzyelwood37@gmail.com and password as IzzyColon2024 and then press the login button')
+    cache_dir = await screen.build_action('enter email as grand.hunter.dark.15@gmail.com and then press the next button')
     await execute.load(cache_dir).call()
     await asyncio.sleep(5)
 
-    cache_dir = await screen.build_action('select user airborn')
+    cache_dir = await screen.build_action('enter the password as Grandhunter15 and press the login button')
     await execute.load(cache_dir).call()
-    await asyncio.sleep(3)
+    await asyncio.sleep(5)
 
-    cache_dir = await screen.build_action('send message "hello from agent"')
+    cache_dir = await screen.build_action('press the compose button')
     await execute.load(cache_dir).call()
-    await asyncio.sleep(3)
+    await asyncio.sleep(5)
+
+    cache_dir = await screen.build_action('enter balajirw10@gmail.com in the To field')
+    await execute.load(cache_dir).call()
+    await asyncio.sleep(5)
+
+    cache_dir = await screen.build_action('enter Test in the Subject field')
+    await execute.load(cache_dir).call()
+    await asyncio.sleep(5)
+
+    cache_dir = await screen.build_action('Enter "I love cats" in the body field')
+    await execute.load(cache_dir).call()
+    await asyncio.sleep(5)
+
+    cache_dir = await screen.build_action('send the email')
+    await execute.load(cache_dir).call()
+    await asyncio.sleep(5)
 
 
 if __name__ == '__main__':
