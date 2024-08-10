27178148468ee2c8de0fc7d015eb655c5a3636f5
Tata0703 <danqing.zhang.personal@gmail.com>
2024-08-10 13:44:18 -0700

    clean up: remove unused folders"

--- frameworks/litewebagent/observation/constants.py
+++
@@ -1,7 +0,0 @@
-TEXT_MAX_LENGTH = 2**32 - 1
-
-BROWSERGYM_ID_ATTRIBUTE = "data-testid"  # Playwright's default is "data-testid"
-BROWSERGYM_VISIBILITY_ATTRIBUTE = "browsergym_visibility_ratio"
-BROWSERGYM_SETOFMARKS_ATTRIBUTE = "browsergym_set_of_marks"
-
-EXTRACT_OBS_MAX_TRIES = 5
--- frameworks/personal-companion/flows/activate.py
+++
@@ -1,5 +0,0 @@
-from playwright.async_api import Page
-
-class ActivateFlow:
-    def __init__(self, page: Page):
-        self.page = Page
