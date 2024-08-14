50955a070fe2446a52f3df56f7f02bbab55ed291
BRama10 <grand.hunter.dark.15@gmail.com>
2024-08-14 14:50:47 -0400

    react agent

--- litewebagent/agents/ReActAgent.py
+++ litewebagent/agents/ReActAgent.py
@@ -13,5 +13,49 @@ class ReActAgent(BaseAgent):
         self.plan_max_fail_times = 3
         self.tool_call_max_fail_times = 3
 
-    def send_completion_request(self, plan: str, depth: int = 0) -> Dict:
-        raise NotImplementedError("This method should be implemented by subclasses")
+    def check_workflow(self, message):
+        try:
+            # print(f"Workflow message: {message}")
+            workflow = json.loads(message)
+            if not isinstance(workflow, list):
+                return None
+
+            for step in workflow:
+                if "message" not in step or "tool_use" not in step:
+                    return None
+
+            return workflow
+
+        except json.JSONDecodeError:
+            return None
+
+    def automatic_workflow(self):
+        for i in range(self.plan_max_fail_times):
+            response = self.get_response(
+                query = Query(
+                    messages = self.messages,
+                    tools = None,
+                    message_return_type="json"
+                )
+            )
+
+
+            workflow = self.check_workflow(response.response_message)
+
+            self.rounds += 1
+
+            if workflow:
+                return workflow
+
+            else:
+                self.messages.append(
+                    {
+                        "role": "assistant",
+                        "content": f"Fail {i+1} times to generate a valid plan. I need to regenerate a plan"
+                    }
+                )
+        return None
+
+    def send_completion_request(self, plan: str | None, depth: None = None) -> Dict:
+        if plan is None:
+            plan = self.make_plan()
