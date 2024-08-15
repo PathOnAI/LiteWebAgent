52211cb86b25c4e61241493d73ad92dd9fb66c4d
Tata0703 <danqing.zhang.personal@gmail.com>
2024-08-14 22:20:50 -0700

    refactor: only keep one main.py file

--- litewebagent/main.py
+++ litewebagent/main.py
@@ -20,13 +20,6 @@ logger = logging.getLogger(__name__)
 
 from litewebagent.webagent import setup_web_agent
 
-def driver_main(q):
-    start = time.time()
-    output = main()
-    end = time.time()
-    runtime = end - start
-    q.put((runtime, output))
-
 def main(args):
     # litewebagent/playground/workflow_generation
     import json
@@ -36,16 +29,9 @@ def main(args):
     page = playwright_manager.get_page()
     playwright_manager.playwright.selectors.set_test_id_attribute('data-unique-test-id')
 
-    starting_url = "https://www.google.com"
-
-    plan = "search dining table"
-    goal = "search dining table"
-    agent = setup_web_agent(starting_url, goal, model_name=args.model, agent_type=args.agent_type)
-    response = agent.send_prompt(plan)
+    agent = setup_web_agent(args.starting_url, args.goal, model_name=args.model, agent_type=args.agent_type)
+    response = agent.send_prompt(args.plan)
     print(response)
-    # response = use_web_agent(starting_url, goal, plan, agent_type=args.agent_type)
-    # print(response)
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run web automation tasks with different agent types.")
@@ -54,7 +40,11 @@ if __name__ == "__main__":
                         help="Type of agent to use (default: DemoAgent)")
     parser.add_argument('--model', type=str, default="gpt-4o-mini",
                         help="Model to use for the agent (default: gpt-4o-mini)")
-    parser.add_argument('--workflow_index', type=int, default=5,
-                        help="Index of the workflow to use from the JSON file (default: 5)")
+    parser.add_argument('--starting_url', type=str, required=True,
+                        help="Starting URL for the web automation task")
+    parser.add_argument('--plan', type=str, required=True,
+                        help="Plan for the web automation task")
+    parser.add_argument('--goal', type=str, required=True,
+                        help="Goal for the web automation task")
     args = parser.parse_args()
     main(args)
