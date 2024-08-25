81b43a33d01615a8e1cfbbb5f7638a00c139e9e9
Tata0703 <danqing.zhang.personal@gmail.com>
2024-08-24 17:36:26 -0700

    release on pypi: https://pypi.org/project/litewebagent/, version 0.1.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='litewebagent',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     include_package_data=True,
     author='Danqing Zhang',
