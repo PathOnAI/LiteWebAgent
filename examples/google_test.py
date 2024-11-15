7732542be33d53d6db722ca01cd6ca46ce4a8b19
Tata0703 <danqing.zhang.personal@gmail.com>
2024-11-14 22:35:15 -0800

    update examples

--- examples/google_test.py
+++ examples/google_test.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 
 _ = load_dotenv()
 from litewebagent.core.agent_factory import setup_function_calling_web_agent
+from litewebagent.utils.playwright_manager import setup_playwright
 
 agent_type = "FunctionCallingAgent"
 starting_url = "https://www.google.com"
@@ -13,7 +14,8 @@ features = "axtree"
 branching_factor = None
 storage_state = None
 
-agent = setup_function_calling_web_agent(starting_url, goal, model_name=model, agent_type=agent_type, features=features,
-                        tool_names = ["navigation", "select_option", "upload_file", "webscraping"], branching_factor=branching_factor, log_folder=log_folder, storage_state=storage_state)
+playwright_manager = setup_playwright(log_folder=log_folder, storage_state='state.json', headless=False)
+agent = setup_function_calling_web_agent(starting_url, goal, playwright_manager=playwright_manager, model_name=model, agent_type=agent_type, features=features,
+                        tool_names = ["navigation", "select_option", "upload_file", "webscraping"], branching_factor=branching_factor, log_folder=log_folder)
 response = agent.send_prompt(plan)
 print(response)
